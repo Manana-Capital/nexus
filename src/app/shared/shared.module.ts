6bcc62ee854f6f1ef667519b4c11e2cc7f18a38b
Mariusz Kotas <m@mkotas.cz>
2019-06-07 21:52:24 +0200

    Implement authentication directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,13 +10,14 @@ import {NgxChartsModule} from '@swimlane/ngx-charts';
 import { NxTrendComponent } from './components/nx-trend/nx-trend.component';
 import {NxCardComponent} from './components/nx-card/nx-card.component';
 import {ExternalViewComponent} from './components/external-view-component/external-view.component';
-import {NxGravatarService} from '@core/services/nx-gravatar.service';
+import { NxAuthDirective } from './directive/nx-auth.directive';
 
 const exportedComponents = [
   ExternalViewComponent,
   UserHeadComponent,
   NxCardComponent,
-  NxTrendComponent
+  NxTrendComponent,
+  NxAuthDirective
 ];
 
 const exportedModules = [
