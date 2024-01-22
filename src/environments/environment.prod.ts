67f4973c2c34fd336999df9d1e4538b2d0d3ac61
Mariusz Kotas <m@mkotas.cz>
2024-01-22 15:03:41 +0100

    Change backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   SERVER_URL: `./`,
-  BACKEND_URL: `https://kotas.host/`,
+  BACKEND_URL: `https://nexus.kotas.host/`,
   production: true,
   useHash: true,
   hmr: false,
